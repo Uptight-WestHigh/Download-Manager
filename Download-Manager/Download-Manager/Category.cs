b677610d4875ef72fe4c7206b5492a00d5c04432
Zufixx <filip.0717@hotmail.com>
2017-03-30 16:08:55 +0200

    Initial category code.

---
+++ Download-Manager/Download-Manager/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Download_Manager
+{
+    public class Category
+    {
+        public string name;
+        public int id;
+
+        public Category(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
---
+++ Download-Manager/Download-Manager/Listing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Download_Manager
+{
+    public class Listing
+    {
+        public List<Category> categories = new List<Category>();
+        public string[] categoryName = new string[] 
+        {   "Web Browsers",
+            "File Sharing",
+            "Compression",
+            "Messaging",
+            "Online Storage",
+            "Media",
+            "Runtimes",
+            "Other",
+            "Imaging",
+            "Developer Tools",
+            "Documents",
+            "Utilities",
+            "Security" };
+
+        public void Initialize()
+        {
+            Category webBrowsers = new Category("Web Browsers", 0);
+            categories.Add(webBrowsers);
+
+            for (int i = 0; i < 12; i++)
+            {
+                Category category = new Category(categoryName[i], i);
+                categories.Add(category);
+            }
+        }
+    }
+}
